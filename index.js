e8028245877991fcd91a1525e6fb57c27ed68706
f12998765 <f12998765@yeah.net>
2016-07-29 21:28:00 +0800

    edit url.json url error #4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function(){
-    $.getJSON("http://f12998765.coding.me/SpeedDial/url.json",function(data){
+    $.getJSON("/url.json",function(data){
         var gettpl = $('#demo').html();
 laytpl(gettpl).render(data, function(html){
     $('#view').html(html);
