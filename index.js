16f6afe96072a1559b9b96cb17dd5fd7a30482d3
f12998765 <f12998765@yeah.net>
2016-07-30 11:06:56 +0800

    add <a> target="_blank"

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(function(){
     $.getJSON("/url.json",function(data){
         var gettpl = $('#demo').html();
-laytpl(gettpl).render(data, function(html){
-    $('#view').html(html);
-});
+        laytpl(gettpl).render(data, function(html){
+            $('#view').html(html);
+        });
     }
-        );})
+);})
