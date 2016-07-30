aa2f6fdbf290fc604a16d161858dde99baf73004
f12998765 <f12998765@yeah.net>
2016-07-30 19:30:30 +0800

    Test qiniu

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function(){
-    $.getJSON("/url.json",function(data){
+    $.getJSON("http://ob4kd8p3g.bkt.clouddn.com/url.json",function(data){
         var gettpl = $('#demo').html();
         laytpl(gettpl).render(data, function(html){
             $('#view').html(html);
