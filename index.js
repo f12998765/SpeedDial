92509a2b63b80f9c6e77449f7f5ab51144ecf893
f12998765 <f12998765@yeah.net>
2016-08-04 09:06:40 +0800

    响应式设计

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 $(function(){
+
+     var isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i) ? true : false;
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i) ? true : false;
+        },
+        any: function () {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+        }
+    };
+
     $.getJSON("https://ob4kd8p3g.bkt.clouddn.com/url.json",function(data){
-        var gettpl = $('#demo').html();
-        laytpl(gettpl).render(data, function(html){
-            $('#view').html(html);
-        });
-    }
-);})
+        var gettpl = $("#demo").html();
+        laytpl(gettpl).render(data, function(html){$('#view').html(html);})
+    })
+    .fail(function(){
+            $.getJSON("/url.json",function(data){
+                var gettpl = $('#demo').html();
+                laytpl(gettpl).render(data, function(html){ $('#view').html(html);})
+            })
+            .fail(function(){
+                alert("加载失败!")
+            });
+        })
+    .always(function(){ 
+        $(".block").addClass('color');
+        if (isMobile.any()) { 
+            $(".hh").css('width','100%');
+            $(".hi").removeClass('tit');
+            $(".block").removeClass('color');
+            $(".hh").addClass('color');
+        } 
+    });
+
+    });
