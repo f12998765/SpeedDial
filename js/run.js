9ce336879f7e62f9267a725518d8dbee1a888149
zero <f12998765@yeah.net>
2018-04-07 17:17:26 +0800

    新主题

--- js/run.js
+++ js/run.js
@@ -14,4 +14,15 @@ onload = function () {
     document.getElementById("text").onkeypress = function (e) {
         if(e.which==13) run();
     }
+    var ul = document.getElementById("ul");
+    for(var i in urls){
+        for(var j in urls[i].list){
+            var li = document.createElement("li");
+            var a = document.createElement("a");
+            a.innerHTML=urls[i].list[j].name;
+            a.href=urls[i].list[j].url;
+            li.appendChild(a);
+            ul.appendChild(li);
+        }
+    }
 }
