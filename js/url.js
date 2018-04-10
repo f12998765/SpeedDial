eaa870563af2f706983ff6a1b52290b66253e25e
zero <f12998765@yeah.net>
2018-04-10 17:14:45 +0800

    更新url

--- js/url.js
+++ js/url.js
@@ -1,5 +1,13 @@
 var urls = [
   {
+    "name": "LeetCode",
+    "url": "https://leetcode-cn.com/"
+  },
+  {
+    "name": "wiki",
+    "url": "https://wiki.xizero.com/"
+  },
+  {
     "name": "V2EX",
     "url": "https://v2ex.com/"
   },
@@ -8,42 +16,19 @@ var urls = [
     "url": "https://juejin.im/"
   },
   {
-    "name": "wiki",
-    "url": "https://wiki.xizero.com/"
-  },
-  {
     "name": "Github",
     "url": "https://github.com/f12998765"
-  },
-  {
-    "name": "GitBook",
-    "url": "https://www.gitbook.com/explore"
   },
   {
     "name": "翻译",
     "url": "https://translate.google.cn/"
   },
   {
-    "name": "Twitter",
-    "url": "https://twitter.com/",
-    "img": "https://ox.xizero.com/uploads/2017/06/twitter_.png"
-  },
-  {
     "name": "哔哩哔哩",
     "url": "https://www.bilibili.com/"
   },
   {
-    "name": "蒸汽动力",
-    "url": "https://steamcn.com/forum.php"
-  },
-  {
     "name": "Product Hunt",
     "url": "https://www.producthunt.com/"
-  },
-  {
-    "name": "LeetCode",
-    "url": "https://leetcode-cn.com/"
   }
 ]
-
-
