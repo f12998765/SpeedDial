de0dec9e647707f0ecbbd568b849870abd72ac5f
zero <f12998765@yeah.net>
2018-06-14 10:08:24 +0800

    更改css插入时间

--- js/run.js
+++ js/run.js
@@ -1,4 +1,4 @@
-function run() {
+function run () {
     var text = document.getElementById("text").value;
     var so = "https://www.google.com.hk/search?q=";
     if (text != "") {
@@ -12,33 +12,34 @@ function run() {
 
 
 onload = function () {
+    var theme = localStorage.getItem("theme");
+    if (theme == "" || theme == undefined)
+        localStorage.setItem("theme", 1);
+    change_theme(false);
+
     document.getElementById("sub").onclick = run;
     document.getElementById("text").onkeypress = function (e) {
-        if(e.which==13) run();
+        if (e.which == 13) run();
     }
     var ul = document.getElementById("ul");
-    for(var i in urls){
+    for (var i in urls) {
         var li = document.createElement("li");
         var a = document.createElement("a");
-        a.innerHTML=urls[i].name;
-        a.href=urls[i].url;
+        a.innerHTML = urls[i].name;
+        a.href = urls[i].url;
         li.appendChild(a);
         ul.appendChild(li);
     }
-    var theme = localStorage.getItem("theme");
-    if(theme==""||theme==undefined)
-        localStorage.setItem("theme",1);
-    change_theme(false);
 }
 
-var themes = ["light","night","nyan"];
+var themes = ["light", "night", "nyan"];
 
-function change_theme(flag){
+function change_theme (flag) {
     var n = localStorage.getItem("theme");
-    if(flag){
-        n++;n%=themes.length;
-        localStorage.setItem("theme",n);
+    if (flag) {
+        n++; n %= themes.length;
+        localStorage.setItem("theme", n);
     }
-    var css  = document.getElementById("css");
-    css.href = "theme/"+themes[n]+"/css/style.css";
+    var css = document.getElementById("css");
+    css.href = "theme/" + themes[n] + "/css/style.css";
 }
